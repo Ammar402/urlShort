44ff248a644aed791be30bc69302875e7d18fef5
Glitch (spiffy-bacon) <none>
2018-03-28 04:30:21 +0000

    🌗👷 Checkpoint ./views/index.html:774464/120 ./app.js:774464/129 ./public/client.js:774464/601 ./package.json:774464/98 ./server.js:774464/3

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + 'public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
