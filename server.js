bdc9049d88ad3dfd328941b76f436ca8e0ed2ada
Glitch (spiffy-bacon) <none>
2018-03-29 02:12:10 +0000

    💾🏬 Checkpoint ./server.js:774464/1

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (request, response) => {
 })
 
 // Simple in-memory store
-app.get('/new/urlToShorten(*)',(req,res,next)=>{
+app.get('/new/:urlToShorten(*)',(req,res,next)=>{
   var urlToShorten = req.params.urlToShorten;
   
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
